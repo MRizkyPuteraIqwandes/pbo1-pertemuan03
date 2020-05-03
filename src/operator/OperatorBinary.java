8cb81de0dfd0fd1494d3ac04d14a78823fcd9ef7
Thalia5 <Thalia5@users.noreply.github.com>
2020-05-03 22:37:34 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,21 @@
+ package operator;
+
+    public class OperatorBinary {
+
+        //operator (=) sebagai assigment/penugasan
+        //menempatkan nilai kedalam variabel
+
+        String nama = "Thalia";
+        int jumlahSks = 24;
+        double indeksPresentasiKumulatif = 3.99;
+
+        public static void main(String[] args ){
+            System.out.println(9+3);
+            System.out.println(9-3);
+            System.out.println(9*3);
+            System.out.println(9/3);
+            System.out.println(9%3);
+            System.out.println(9%2);
+        }
+
+    }
